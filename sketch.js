075d82b53c41e6b4cea2ea40fb34913ef5f8f6be
moc14 <mia@D-69-91-177-192.dhcp4.washington.edu>
2017-02-05 13:11:32 -0800

    Drawing Poo

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var containerY = 0;
 
 var img;
 var img3;
+var poop;
+var poopImg;
 
 
 //var moveCorgi;
@@ -21,6 +23,12 @@ var snowflakes = [
 // Defining list of bone array
 var bone = [
 	{ x: 10, y: 10, size: 5, v: 5},
+];
+
+
+// Defining list of poop array
+var poop = [
+	{ x: 90, y: 50, size: 5, v: 5},
 ];
 
 
@@ -48,15 +56,31 @@ function setup () {
     
 
     // Randomly Generate bone
-	//for (var i = 0; i < 100; i++) { var boneX = random(containerW); // where to start on x
-    //var boneY = random((containerH * 1)); // where to start on y
-		//var boneS = random(3, 8); // how big to be
-		//var boneV = random(2, 5); // how fast to move
+	for (var i = 0; i < 100; i++) {
+		var boneX = random(containerW); // where to start on x
+		var boneY = random((containerH * 1)); // where to start on y
+		var boneS = random(3, 8); // how big to be
+		var boneV = random(2, 5); // how fast to move
 
-	//}
+	}
     
      // Add each bone as it is generated to the bones
-//bone.push({ x: boneX, y: boneY, size: boneS, v: boneV });
+		bone.push({ x: boneX, y: boneY, size: boneS, v: boneV });
+    
+    
+    // Randomly Generate poop
+	for (var i = 0; i < 100; i++) {
+		var poopX = random(containerW); // where to start on x
+		var poopY = random((containerH * 1)); // where to start on y
+		var poopS = random(3, 8); // how big to be
+		var poopV = random(2, 5); // how fast to move
+
+	}
+    
+     // Add each poop as it is generated to the poops
+		poop.push({ x: poopX, y: poopY, size: poopS, v: poopV });
+    
+    
     
     
 	// Create new canvas element within section#Display
@@ -71,6 +95,9 @@ function setup () {
     //load image of bone
     img3 =loadImage("img/BONE3.png");
 
+    
+      //load image of poop
+    poopImg =loadImage("img/poop.png");
     
     //initiating x coordinates for corgi
     //moveCorgi = 0;
@@ -96,9 +123,6 @@ function draw () {
 			flake.y = 0;
 		}
 
-             
-        
-        
 		// draw snowflake
 		ellipse(flake.x, flake.y, flake.size, flake.size);
 	}
@@ -115,13 +139,27 @@ function draw () {
 			treat.y = 0;
 		}
 
-        if ( treat.x > containerW ) {
-        treat.x = random(windowWidth);}
-
 		// draw bone
 		image(img3, treat.x, treat.y, treat.width/9, treat.height/10);
+        
+        // for each poop
+	for (var i = 0; i < bone.length; i++) {
+		var shit = poop[i]; // define current bone
+		
+		// update treat y coordinate based on treat velocity
+		shit.y = shit.y + shit.v;
+
+		// if flake is beyond bottom of the canvas element, start it at the top
+		if ( shit.y > containerH ) {
+			shit.y = 0;
+		}
+
+		// draw bone
+		image(poopImg, shit.x, shit.y, shit.width/9, shit.height/10);
+        
+        
 	}
-    
+}
     
     
     
